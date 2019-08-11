d9cdb53690838030e752656d27b727abe66ce310
Jaclyn Hall <jaclynchall@gmail.com>
2019-08-11 17:15:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,9 @@ class Song
 
   def initialize
     @@song_count += 1
-    attr_accessor :name 
-    attr_accessor :genre
-    attr_accessor :artist
-  end
 
   def self.count
     @@song_count
   end
-  
+
 end
