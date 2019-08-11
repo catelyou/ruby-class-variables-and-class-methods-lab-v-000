f1e6fde4ce9a9bcb0ee5ad1b6735ba16fff2b2fe
Jaclyn Hall <jaclynchall@gmail.com>
2019-08-11 17:44:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,15 +26,7 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre| 
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre]
-      end
-    end
-    genre_count
+  
   end
 
 end
